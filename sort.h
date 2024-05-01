403f66aa4542bcbba2bdcd9f600e32fbb08e8f59
Omar Fathi22 <ommarf525@gmail.com>
2024-05-01 06:28:24 -0700

    106-bitonic_sort.c

--- sort.h
+++ sort.h
@@ -32,5 +32,7 @@ void swap_nodes(listint_t **list, listint_t *left, listint_t *right);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 #endif 
 
